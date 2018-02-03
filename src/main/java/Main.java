d176b7bd74d6638e2cc55869c877c6121efda084
ゼクロ <zekrommaster110@aol.de>
2018-02-03 11:56:15 +0100

    part 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,78 @@
 public class Main {
 
+    /*
+    *   1. Datentypen
+    *   2. Keywords, Access Types
+    *   3. Methods
+    * */
+
+    /*
+        <--[ Variablen Typen ]-->
+     */
+    short myShort = 2;
+    static int myInt = 123;
+    long myLong = 123123123123l;
+
+    float myFloat = 1.3456f;
+    double myDouble = 1.2346;
+
+    String myString = "Hey! This is my string!";
+
+    /*
+        <--[ Arrays ]-->
+     */
+    float myFloatArray[] = {1, 2, 3, 1.3456f};
+    String myStringArray[] = {"hey!", "ho!"};
+    int myIntArray;
+
+    /*
+        <--[ Methoden / Getter & Setter ]-->
+     */
+
+    private static int myPrivateInt;
+
+    public static int getMyPrivateInt() {
+        return myPrivateInt;
+    }
+
+    public static void setMyPrivateInt(int setter) {
+        myPrivateInt = setter;
+    }
+
+
+    public int myPublicInt;
+
+    /*
+        <--[ Access Type: 'final' ]-->
+     */
+
+    private static final String MYSTATICFINASTRING = "Hey ho";
+
+
+    /*
+        Static Voids
+     */
+
+    public static void hello(String name, String secondname) {
+        System.out.println("Hello, " + name + " and " + secondname);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.print("This is my first Java Program!");
+        // System.out.println("Hello World!");
+        // System.out.print("This is my first Java Program!");
+
+        hello("Ringo", "Martin");
+
+        // MYSTATICFINASTRING = "asd"; -> ERROR
+
+        System.out.println(Integer.toString(myInt));
+
+        setMyPrivateInt(17);
+
+        System.out.println(getMyPrivateInt());
+
+        int myNewInt = getMyPrivateInt();
     }
 
 }
+

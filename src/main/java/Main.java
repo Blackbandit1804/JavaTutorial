8614481346dcc946fddf7c7c9318f7e1d286d6b8
zekro <riho@live.de>
2018-05-18 15:36:26 +0200

    [Added stuff for part 4]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,84 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+
 public class Main {
 
-    public static void main(String[] args) {
-        
-        User user = new User(101, "Olaf");
+    static Student table[] = {
+        new Student("Lukas", 0),
+        new Student("Hannes", 1),
+        new Student("Alex", 2)
+    };
 
-        System.out.println(
-            "Name: " + user.getName() +
-            "\nID: " + user.id
-        );
 
-        Member member = new Member(201, "Günter");
-        member.setNick("Günni");
-
-        System.out.println(
-            "Name: " + member.getName() +
-            "\nID: " + member.id +
-            "\nNick: " + member.getNick()
-        );
-
-        System.out.printf(
-                "\nDefault name: " + User.DEFAULT_NAME +
-                "\nName static: " + User.getNameStatic(user)
-        );
-
+    static String readInpt() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 
+    public static void main(String[] args) {
+
+        // TEIL A ------------------------------------
+
+        if (table[0].getName() == "Lukas") {
+            System.out.println("This is Lukas!");
+        }
+        else {
+            System.out.println("This is not Lukas.");
+        }
+
+
+        // TEIL B ------------------------------------
+
+        System.out.print("Who you are searching for?\n> ");
+        String target = readInpt();
+        boolean foundTarget = false;
+
+        // --------------------
+        // While Loop Method
+
+        int counter = 0;
+        while (counter < table.length) {
+            // table[counter].getName() == target -> DOES NOT WORK
+            if (table[counter].getName().equals(target)) {
+                System.out.println("Found " + target + " at position " + counter);
+                foundTarget = true;
+            }
+            counter++;
+        }
+
+        // --------------------
+        // For Loop Method
+
+        for (int i = 0; i < table.length; i++) {
+            if (table[i].getName().equals(target)) {
+                System.out.println("Found " + target + " at position " + i);
+                foundTarget = true;
+            }
+        }
+
+        // --------------------
+        // ForEach Loop Method
+
+        for (Student student : table) {
+            if (student.getName().equals(target)) {
+                System.out.println("Found " + target);
+                foundTarget = true;
+            }
+        }
+
+        // --------------------
+        // Lambda Loop Method
+
+        if (Arrays.stream(table).filter(u -> u.getName().equals(target)).count() > 0) {
+            System.out.println("Found " + target);
+            foundTarget = true;
+        }
+
+        // --------------------
+
+        if (!foundTarget)
+            System.out.println(target + " could not be found!");
+    }
 }
---
+++ src/main/java/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+
+    private String m_name;
+    private int m_id;
+
+    public Student(String name, int id) {
+        m_name = name;
+        m_id = id;
+    }
+
+    public String getName() {
+        return m_name;
+    }
+
+    public int getId() {
+        return m_id;
+    }
+
+}
